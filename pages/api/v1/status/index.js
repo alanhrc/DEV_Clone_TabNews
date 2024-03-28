ec41f81cc8e639fa3768725041754bdd09bf55af
Alan Henrique <alancamargo50@gmail.com>
2024-03-28 13:39:47 -0300

    feat(auto-docker-start): add docker start on run dev script [TABNEWS-XXX]

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
-  response.status(200).json({ chave: 'valor' });
+import database from 'infra/tabnews-database/database.js';
+
+async function status(request, response) {
+  // await database.query('SELECT 1 + 1 as sum;');
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  // console.error(result.rows);
+  response.status(200).json({ chave: `SELECT 1 + 1 = ${result.rows}` });
 }
 
 export default status;
