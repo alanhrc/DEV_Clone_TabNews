614b5e318c53f98bc22b04165442e24aa36f30ea
Alan Henrique <alancamargo50@gmail.com>
2024-04-02 16:35:33 -0300

    feat(/status): return infos database like connections and version [TABNEWS-XXX]

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,41 @@
 import database from 'infra/tabnews-database/database.js';
 
 async function status(request, response) {
-  // await database.query('SELECT 1 + 1 as sum;');
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  // console.error(result.rows);
-  response.status(200).json({ chave: `SELECT 1 + 1 = ${result.rows}` });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResultDetails =
+    await database.query('SELECT version();');
+  const databaseVersionValueDetails =
+    databaseVersionResultDetails.rows[0].version;
+
+  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;',
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_details: databaseVersionValueDetails,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
